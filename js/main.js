81adf66f83cbe130c249b8ac7925f239191972c0
Daria739 <juicebox739@gmail.com>
2019-05-13 11:55:56 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -18,70 +18,77 @@ const popupLogin = document.querySelector('.popup-login');
 const closeBtn = document.querySelector('.popup-close');
 let opacity = 0.2;
 
-openBtn.addEventListener('click', function () {
-    popupLogin.classList.add('popup-login_active');
-    popupLogin.style.opacity = 0;
-
-    setTimeout(function foo() {
-        if (opacity < 1) {
-            opacity += 0.2;
-            popupLogin.style.opacity = opacity;
-            setTimeout(foo, 80);
-        }
-
-
-    }, 80);
-
-
-
-
-    closeBtn.addEventListener('click', function () {
-        setTimeout(function fooC() {
-            if (opacity > 0) {
-                opacity -= 0.2;
-                popupLogin.style.opacity = opacity;
-                setTimeout(fooC, 80);
-            } else {
+    openBtn.addEventListener('click', function () {
+        popupLogin.classList.add('popup-login_active');
+        popupLogin.style.opacity = 0;
     
-                popupLogin.classList.remove('popup-login_active');
+        setTimeout(function foo() {
+            if (opacity < 1) {
+                opacity += 0.2;
+                popupLogin.style.opacity = opacity;
+                setTimeout(foo, 80);
             }
+    
+    
         }, 80);
+    
+    
+    
+    
+        closeBtn.addEventListener('click', function () {
+            setTimeout(function fooC() {
+                if (opacity > 0) {
+                    opacity -= 0.2;
+                    popupLogin.style.opacity = opacity;
+                    setTimeout(fooC, 80);
+                } else {
+        
+                    popupLogin.classList.remove('popup-login_active');
+                }
+            }, 80);
+        
+        
+        });
+    
+        document.addEventListener('keydown', function (e) {
+    
+            setTimeout(function fooC() {
+                if (e.keyCode === 27) {
+                    opacity -= 0.2;
+                    popupLogin.style.opacity = opacity;
+                    setTimeout(fooC, 80);
+                } else {
+                    popupLogin.classList.remove('popup-login_active');
+                }
+            }, 80);
+        
+        });
+    
+        overlayLogin.addEventListener('click', function() {
+            setTimeout(function fooC() {
+                if (opacity > 0) {
+                    opacity -= 0.2;
+                    popupLogin.style.opacity = opacity;
+                    setTimeout(fooC, 80);
+                } else {
+        
+                    popupLogin.classList.remove('popup-login_active');
+                }
+            }, 80);
+        });
+    
+    
+    
     
     
     });
 
-    document.addEventListener('keydown', function (e) {
-
-        setTimeout(function fooC() {
-            if (e.keyCode === 27) {
-                opacity -= 0.2;
-                popupLogin.style.opacity = opacity;
-                setTimeout(fooC, 80);
-            } else {
-                popupLogin.classList.remove('popup-login_active');
-            }
-        }, 80);
-    
-    });
-
-    overlayLogin.addEventListener('click', function() {
-        setTimeout(function fooC() {
-            if (opacity > 0) {
-                opacity -= 0.2;
-                popupLogin.style.opacity = opacity;
-                setTimeout(fooC, 80);
-            } else {
-    
-                popupLogin.classList.remove('popup-login_active');
-            }
-        }, 80);
-    });
 
 
 
 
 
-});
+
 
 // Окно - правила
 
@@ -151,3 +158,81 @@ openBtnRules.addEventListener('click', function () {
 
 
 });
+
+// Кнопка bet 
+
+const betBtn = document.querySelector('.btn-bet');
+const overlayBet = document.querySelector('.overlay-bet');
+const popupBet = document.querySelector('.popup-bet');
+const closeBtnBet = document.querySelector('.popup-close-bet');
+let opacityBet = 0.2;
+
+betBtn.addEventListener('click', function () {
+        popupBet.classList.add('popup-bet_active');
+        popupBet.style.opacityBet = 0;
+    
+        setTimeout(function foo() {
+            if (opacityBet < 1) {
+                opacityBet += 0.2;
+                popupBet.style.opacityBet = opacityBet;
+                setTimeout(foo, 80);
+            }
+    
+    
+        }, 80);
+    
+    
+    
+    
+        closeBtnBet.addEventListener('click', function () {
+            setTimeout(function fooC() {
+                if (opacityBet > 0) {
+                    opacityBet -= 0.2;
+                    popupBet.style.opacityBet = opacityBet;
+                    setTimeout(fooC, 80);
+                } else {
+        
+                    popupBet.classList.remove('popup-bet_active');
+                }
+            }, 80);
+        
+        
+        });
+    
+        document.addEventListener('keydown', function (e) {
+    
+            setTimeout(function fooC() {
+                if (e.keyCode === 27) {
+                    opacityBet -= 0.2;
+                    popupBet.style.opacityBet = opacityBet;
+                    setTimeout(fooC, 80);
+                } else {
+                    popupBet.classList.remove('popup-bet_active');
+                }
+            }, 80);
+        
+        });
+    
+        overlayBet.addEventListener('click', function() {
+            setTimeout(function fooC() {
+                if (opacityBet > 0) {
+                    opacityBet -= 0.2;
+                    popupBet.style.opacity = opacityBet;
+                    setTimeout(fooC, 80);
+                } else {
+        
+                    popupBet.classList.remove('popup-bet_active');
+                }
+            }, 80);
+        });
+    
+    
+    
+    
+    
+    });
+
+
+
+
+
